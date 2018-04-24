7f1ef1b0b514e80c7f113b86715944f83cd81790
Tim C <timoman8@hotmail.com>
2018-04-24 13:21:43 -0400

    Create draft of site

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function Main(){
+    let app = angular.module('body', ['ngAnimate']);
+    app.controller('bodyController', [
+        '$scope',
+        function($scope) {
+            $scope.currentTab = 'home';
+            $scope.checkTab = function(name){
+                if($scope.currentTab == name){
+                    return 'disabled';
+                }
+            };
+            $scope.header = {
+                url: '/header.html'
+            };
+            $scope.footer = {
+                url: '/footer.html'
+            };
+        }
+    ]);
+})();
